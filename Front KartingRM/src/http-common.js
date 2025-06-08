bd803ffac6ca622efff50fee4eef6781fec26aac
Isaac Espinoza Barría <isaac.espinoza.b@usach.cl>
2025-06-08 15:02:35 -0400

    Prueba para conexion de front con back en docker

--- Front KartingRM/src/http-common.js
+++ Front KartingRM/src/http-common.js
@@ -6,7 +6,20 @@ const httpClient = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json"
-  }
+  },
+  withCredentials: false // Importante para CORS
 });
+
+
+// Interceptor para manejar errores globalmente
+httpClient.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response && error.response.status === 403) {
+      console.error("Acceso prohibido - Verifique CORS");
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default httpClient;
